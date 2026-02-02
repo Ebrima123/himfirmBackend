67da2cf14e8627da8613b528db71bf11c2a803d6
Ebrima <esawaneh23@gmail.com>
2026-02-02 07:35:49 +0000

    pushhh

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,7 +1,6 @@
 # crm/serializers.py
 from rest_framework import serializers
 from .models import Customer, Lead, SiteVisit, Allocation
-from core.models import EmployeeProfile
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,20 +8,15 @@ class CustomerSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class LeadSerializer(serializers.ModelSerializer):
-    customer = CustomerSerializer(read_only=True)
     customer_id = serializers.PrimaryKeyRelatedField(
         queryset=Customer.objects.all(),
         source='customer',
         write_only=True
     )
-    assigned_to = serializers.StringRelatedField(read_only=True)
-    assigned_to_id = serializers.PrimaryKeyRelatedField(
-        queryset=EmployeeProfile.objects.all(),
-        source='assigned_to',
-        write_only=True,
-        required=False,
-        allow_null=True
-    )
+    customer = CustomerSerializer(read_only=True)
+
+    assigned_to = serializers.StringRelatedField()
+ 
 
     class Meta:
         model = Lead
